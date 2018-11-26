e197c50a24ef128709fdc29030926da8bea4b985
joammega <joam.mega@gmail.com>
2018-11-26 21:24:48 -0200

    ...

--- Projeto/Site principal/js/calendario.js
+++ Projeto/Site principal/js/calendario.js
@@ -1,1 +1,104 @@
-function 
+let user = localStorage.getItem("user");
+let back = document.querySelector(".back");
+let posts = document.querySelector(".posts");
+let grafico = document.querySelector(".grafico");
+let lista = document.querySelector(".post .lista");
+
+function postagem(data, humor, comentario) {
+  let div = document.createElement("div");
+  let p = document.createElement("p");
+  let img = document.createElement("img");
+  let divaux = document.createElement("div");
+
+  div.classList.add("card");
+
+  divaux.classList.add("data");
+
+  let p_text = document.createTextNode(data);
+  p.appendChild(p_text);
+  divaux.appendChild(p);
+  div.appendChild(divaux);
+
+  divaux = document.createElement("div");
+  divaux.classList.add("humor");
+
+  img.scr = "./imgs/humor/" + humor + ".png";
+  divaux.appendChild(img);
+  div.appendChild(divaux);
+
+  divaux = document.createElement("div");
+  divaux.classList.add("comentario");
+  p = document.createElement("p");
+  p_text = document.createTextNode(comentario);
+
+  p.appendChild(p_text);
+  divaux.appendChild(p);
+  div.appendChild(divaux);
+
+  lista.appendChild(div);
+}
+function grafico(contador) {}
+function getdados(quant) {
+  let xhttp = new XMLHttpRequest();
+  let url = "http://localhost:3000/comments?username=" + user;
+  let conts = [0, 0, 0, 0, 0];
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      let parsed = JSON.parse(xhttp.responseText);
+      if ((quant = "todos")) {
+        for (let i of parsed) {
+          postagem(i.data, i.humor, i.comentario);
+        }
+      } else {
+        for (let i = parsed.length - quant; i < parsed.length; i++) {
+          if (i >= 0) {
+            postagem(parsed[i].data, parsed[i].humor, parsed[i].comentario);
+          }
+        }
+      }
+      for (let i of parsed) {
+        conts[i.humor] += 1;
+      }
+      grafico(conts);
+    }
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  };
+}
+function postdados(data, humor, comentario) {
+  let xhttp = new XMLHttpRequest();
+  let url = "http://localhost:3000/comments";
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      getdados(5);
+    }
+  };
+  let post = {
+    username: user,
+    data: data,
+    humor: humor,
+    comentario: comentario
+  };
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify(post));
+}
+let humor = document.querySelector(".back .funcao .conteudo .humor");
+let calendario = document.querySelector(".back .funcao .conteudo .calendario");
+
+humor.children[2].children[1].addEventListener("click", function() {
+  let dia = "";
+  let mes = calendario.children[0].children[0].innerHTML;
+  let humor = "";
+  let comentario = humor.children[2].children[0].value;
+  for (let i of calendario.children[2].children) {
+    if (i.classList.contains("marcado")) {
+      dia = i.children[0].innerHTML;
+    }
+  }
+  for (let i of humor.children[1]) {
+  }
+  let data = dia + " de " + mes;
+  postdados(data, humor, comentario);
+});
+console.log(humor.children[2].children[0].value);
--- Projeto/Site principal/js/teste.js
+++ Projeto/Site principal/js/teste.js
@@ -1,7 +1,16 @@
 let quests = document.querySelectorAll(
   ".conteiner .conteudo .quadro .quest .caixa ul"
 );
-
+let id = "";
+let user = "";
+if (
+  localStorage.getItem("user") != "" &&
+  localStorage.getItem("user") != null
+) {
+  user = localStorage.getItem("user");
+} else {
+  user = "anonimo";
+}
 for (let i of quests) {
   for (let j = 0; j < i.children.length; j++) {
     i.children[j].addEventListener("click", function() {
@@ -12,4 +21,150 @@ for (let i of quests) {
       i.children[j].classList.add("marcado");
     });
   }
+}
+
+let enviar = document.querySelector(
+  ".conteiner .conteudo .quadro .button button"
+);
+enviar.addEventListener("click", function() {
+  contabilizar();
+});
+function contabilizar() {
+  let cont = 0;
+  for (let i of quests) {
+    for (let j = 0; j < i.children.length; j++) {
+      if (i.children[j].classList.contains("marcado")) {
+        cont += j;
+        console.log("oi");
+        break;
+      } else if (j == 3) {
+        alert("você esqueceu de marcar alguma questão");
+        return;
+      }
+    }
+  }
+  postresult(cont);
+  getresult();
+}
+let conteudo = document.querySelector(".conteiner .conteudo");
+let quadro = document.querySelector(".conteiner .conteudo .quadro");
+function resultado(resultado) {
+  quadro.innerHTML = "";
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let img = document.createElement("img");
+
+  img.src = "./imgs/" + resultado + ".png";
+
+  card.appendChild(img);
+  let div = document.createElement("div");
+  div.classList.add("resposta");
+  let h4 = document.createElement("h4");
+  let h4_text = "";
+
+  let p = document.createElement("p");
+  let p_text = "";
+  if (resultado == 0) {
+    h4_text = document.createTextNode("Seu resultado aponta 'ansiedade'");
+    p_text = document.createTextNode(
+      "Você apresenta sintomas de ansiedade e talvez seja melhor procurar um profissional para ver melhor sua situação."
+    );
+  } else if (resultado == 1) {
+    h4_text = document.createTextNode(
+      "Seu resultado aponta 'chance de ansiedade'"
+    );
+    p_text = document.createTextNode(
+      "Você pode ter ansiedade, mas isso não é certo, talvez seja bom ver a opinião de um profissional."
+    );
+  } else {
+    h4_text = document.createTextNode("Seu resultado aponta 'sem ansiedade");
+    p_text = document.createTextNode(
+      "Você não apresenta sintomas de ansiedade, parece está tudo bem com você, mas não quer dizer que não seja bom passar por um profissional, não é só quando estamos com problemas que precisamos de alguém pra conversar."
+    );
+  }
+  let div1 = document.createElement("div");
+  div1.classList.add("button");
+  let a = document.createElement("a");
+  a.href = "./formulario.html";
+  let button1_text = document.createTextNode("Solicitar ajuda psicológica");
+  a.appendChild(button1_text);
+  div1.appendChild(a);
+
+  let div2 = document.createElement("div");
+  div2.classList.add("button");
+  let button = document.createElement("button");
+  button.href = "./page_teste.html";
+  let button2_text = document.createTextNode("Refazer teste");
+  button.appendChild(button2_text);
+  div2.appendChild(button);
+
+  h4.appendChild(h4_text);
+  p.appendChild(p_text);
+  div.appendChild(h4);
+  div.appendChild(p);
+  div.appendChild(div1);
+  div.appendChild(div2);
+  card.appendChild(div);
+  conteudo.appendChild(card);
+  button.addEventListener("click", function() {
+    deleteresult();
+  });
+}
+
+function postresult(result) {
+  let xhttp = new XMLHttpRequest();
+  let url = "http://localhost:3000/teste";
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      console.log("foi");
+    }
+  };
+  let resultado = {
+    resultado: result,
+    username: user
+  };
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify(resultado));
+}
+
+function getresult() {
+  let xhttp = new XMLHttpRequest();
+  let url = "http://localhost:3000/teste?username=" + user;
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      let parsed = JSON.parse(xhttp.responseText);
+      console.log(parsed);
+      if (parsed.length == 0) {
+        return;
+      } else {
+        id = parsed[parsed.length - 1].id;
+        if (parsed[parsed.length - 1].resultado <= 7) {
+          resultado(0);
+        } else if (parsed[parsed.length - 1].resultado <= 11) {
+          resultado(1);
+        } else {
+          resultado(2);
+        }
+      }
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+if (user != "anonimo") {
+  getresult();
+}
+function deleteresult() {
+  let xhttp = new XMLHttpRequest();
+  let url = "http://localhost:3000/teste/" + id;
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      location.reload();
+    }
+  };
+  xhttp.open("DELETE", url, true);
+  xhttp.send();
 }
